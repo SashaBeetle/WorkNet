7af74be16a3a257fe77747c62378740413dc7a1c
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2025-05-29 00:37:33 +0300

    Add Home, Register and Login page, add Flowbite. working with HTML

--- worknet-frontend/src/app/app.component.ts
+++ worknet-frontend/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './features/home/home/home.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, Router } from '@angular/router';
 import { HeaderComponent } from './features/header/header/header.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HomeComponent, HeaderComponent],
+    imports: [RouterOutlet, HeaderComponent, CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'worknet-frontend';
+  hiddenHeaderRoutes = ['/login', '/register', '/forgot-password']; 
+
+  constructor(public router: Router) {}
 }
